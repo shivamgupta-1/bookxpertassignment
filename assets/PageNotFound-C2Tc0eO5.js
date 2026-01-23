c55839b0012252950ba61b0a1d4dd15a681d6ea3
shivamgupta-1 <guptashivam202000@gmail.com>
2026-01-23 14:34:32 +0530

    Updates

---
+++ assets/PageNotFound-C2Tc0eO5.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BcLWQyYA.js";const n=()=>t.jsx("div",{style:{textAlign:"center",marginTop:"50px"},children:t.jsx("h1",{children:"404 Page Not Found"})});export{n as default};
--- assets/PageNotFound-OvO6EOhF.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-LAcTVUFG.js";const n=()=>t.jsx("div",{style:{textAlign:"center",marginTop:"50px"},children:t.jsx("h1",{children:"404 Page Not Found"})});export{n as default};
